bce8787762755ce3ff95a8f997f36f3d59195340
ganlhi <ganlhi@gmail.com>
2018-11-17 16:18:40 +0100

    Player Manager

---
+++ Void Tactics/Assets/Scripts/PlayersManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayersManager : MonoBehaviour
+{
+    #region Public methods
+
+    public void OnPlayerReady(bool ready)
+    {
+        Debug.Log("Local Player Ready: " + ready);
+    }
+
+    #endregion Public methods
+
+    #region Unity callbacks
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+    }
+
+    #endregion Unity callbacks
+}
