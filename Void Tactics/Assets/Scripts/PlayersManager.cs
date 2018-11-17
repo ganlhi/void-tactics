5af52a89aa71629b36b18c68a28cf4d70388def7
ganlhi <ganlhi@gmail.com>
2018-11-17 17:08:18 +0100

    Wait for player to run turn

--- Void Tactics/Assets/Scripts/PlayersManager.cs
+++ Void Tactics/Assets/Scripts/PlayersManager.cs
@@ -1,20 +1,69 @@
-﻿using UnityEngine;
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
-public class PlayersManager : MonoBehaviour
+[RequireComponent(typeof(PhotonView))]
+public class PlayersManager : MonoBehaviourPunCallbacks
 {
+    #region Private variables
+
+    private Dictionary<int, bool> playersReady = new Dictionary<int, bool>();
+
+    [SerializeField]
+    private BoolGameEvent runningTurnEvent;
+
+    #endregion Private variables
+
     #region Public methods
 
     public void OnPlayerReady(bool ready)
     {
         Debug.Log("Local Player Ready: " + ready);
+        photonView.RPC("RPC_SetPlayerReady", RpcTarget.MasterClient, PhotonNetwork.LocalPlayer.ActorNumber, ready);
     }
 
     #endregion Public methods
+
+    #region Private methods
+
+    [PunRPC]
+    private void RPC_SetPlayerReady(int actorNum, bool ready)
+    {
+        if (!PhotonNetwork.IsMasterClient)
+        {
+            return;
+        }
+
+        Debug.LogFormat("Player {0} Ready: {1}", actorNum, ready);
+        playersReady[actorNum] = ready;
+        CheckPlayersReady();
+    }
+
+    private void CheckPlayersReady()
+    {
+        if (!PhotonNetwork.IsMasterClient)
+        {
+            return;
+        }
+
+        if (playersReady.Values.All(ready => ready))
+        {
+            runningTurnEvent.Raise(true);
+        }
+    }
+
+    #endregion Private methods
 
     #region Unity callbacks
 
     private void Start()
     {
+        if (!PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.OfflineMode = true;
+        }
     }
 
     private void Update()
@@ -22,4 +71,44 @@ public class PlayersManager : MonoBehaviour
     }
 
     #endregion Unity callbacks
+
+    #region Photon callbacks
+
+    public override void OnConnectedToMaster()
+    {
+        if (PhotonNetwork.OfflineMode)
+        {
+            PhotonNetwork.CreateRoom("OfflineRoom");
+        }
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Player in room: " + PhotonNetwork.LocalPlayer.ActorNumber);
+        if (PhotonNetwork.IsMasterClient)
+        {
+            playersReady.Add(PhotonNetwork.LocalPlayer.ActorNumber, false);
+        }
+    }
+
+    public override void OnPlayerEnteredRoom(Player player)
+    {
+        Debug.Log("Player Left: " + player.ActorNumber);
+        if (PhotonNetwork.IsMasterClient)
+        {
+            playersReady.Add(player.ActorNumber, false);
+        }
+    }
+
+    public override void OnPlayerLeftRoom(Player player)
+    {
+        Debug.Log("Player Entered: " + player.ActorNumber);
+        if (PhotonNetwork.IsMasterClient)
+        {
+            playersReady.Remove(player.ActorNumber);
+            CheckPlayersReady();
+        }
+    }
+
+    #endregion Photon callbacks
 }
